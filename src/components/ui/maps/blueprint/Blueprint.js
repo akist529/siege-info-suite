1fa50a05046a7994d137b18e1c9d581c3cc88e0d
Alex Kist <akist0707@gmail.com>
2022-11-28 15:02:16 -0600

    Change from HTML-mapping solution for clickable blueprint to an SVG solution

--- src/components/ui/maps/blueprint/Blueprint.js
+++ src/components/ui/maps/blueprint/Blueprint.js
@@ -1,16 +1,26 @@
 export default function Blueprint(props) {
     const { mapName, floor, floorData } = props
 
+    function handleClick(e) {
+        console.log(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+    }
+
     return (
         <div className="Blueprint">
             <img 
                 alt={`${mapName} Blueprint - ${floor}`} 
-                src={`/images/maps/${mapName}/${mapName}-${floor}`} 
-                usemap="#workmap"
+                src={`/images/maps/${mapName}/${mapName}-${floor}.webp`}
+                onClick={handleClick} 
             />
-            <map name="workmap">
-                { floorData.map(item => <area key={item.room} shape={item.shape} coords={item.coords.flat(1).toString} alt={item.room}/>) }
-            </map>
+            <div className="blueprint-buttons">
+                <svg width="100%" height="100%" viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                    <polygon 
+                        className="blueprint-shape"
+                        onMouseOver={e => e.currentTarget.classList.add("hoverable")} 
+                        onMouseLeave={e => e.currentTarget.classList.remove("hoverable")}
+                        points="463,165 549,165 549,183 712,183 712,165 790,165 790,250 463,250"/>
+                </svg>
+            </div>
         </div>
     )
 }
