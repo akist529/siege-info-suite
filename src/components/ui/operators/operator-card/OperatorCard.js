27ea824d90399119573d98e1f1bd71ce25b018d6
Alex Kist <akist0707@gmail.com>
2022-12-19 20:39:20 -0600

    Add further mobile responsiveness for operator card and page

--- src/components/ui/operators/operator-card/OperatorCard.js
+++ src/components/ui/operators/operator-card/OperatorCard.js
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom'
 export default function OperatorCard(props) {
     const { name, team } = props
     const [showPortrait, setShowPortrait] = useState()
+    const [showTeam, setShowTeam] = useState()
 
     useEffect(() => {
         setShowPortrait(window.innerWidth >= 470 ? true : false)
+        setShowTeam(window.innerWidth >= 380 ? true : false)
     }, [])
 
     function focusCard(e) {
@@ -19,6 +21,7 @@ export default function OperatorCard(props) {
 
     function resizeHandler() {
         setShowPortrait(window.innerWidth >= 470 ? true : false)
+        setShowTeam(window.innerWidth >= 380 ? true : false)
     }
 
     window.addEventListener("resize", resizeHandler)
@@ -28,7 +31,7 @@ export default function OperatorCard(props) {
             <div className="OperatorCard" onMouseOver={focusCard} onMouseLeave={unFocusCard}>
                 <div className="operator-details">
                     <span id="name">{name}</span>
-                    <img id="faction" alt={`${team}`} src={ `images/ui/${team}.webp` } />
+                    { showTeam && <img id="faction" alt={`${team}`} src={ `images/ui/${team}.webp` } /> }
                 </div>
                 { showPortrait && <img id="portrait" alt={`${name}`} src={`images/ops/portraits/${name}.webp`} /> }
                 <img id="badge" alt={`${name}`} src={`images/ops/icons/${name}.webp`} />
